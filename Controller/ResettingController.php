c696f42de4231e7086432f401eb8d9d5f14df9af
Rafał Mikołajun <rafal@mikoweb.pl>
2015-11-06 19:05:25 +0100

    zmiana emaila

--- Controller/ResettingController.php
+++ Controller/ResettingController.php
@@ -4,7 +4,7 @@
  * This file is part of the vSymfo package.
  *
  * website: www.vision-web.pl
- * (c) Rafał Mikołajun <rafal@vision-web.pl>
+ * (c) Rafał Mikołajun <rafal@mikoweb.pl>
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
--- DependencyInjection/vSymfoFOSUserBundleExtension.php
+++ DependencyInjection/vSymfoFOSUserBundleExtension.php
@@ -4,7 +4,7 @@
  * This file is part of the vSymfo package.
  *
  * website: www.vision-web.pl
- * (c) Rafał Mikołajun <rafal@vision-web.pl>
+ * (c) Rafał Mikołajun <rafal@mikoweb.pl>
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
@@ -16,7 +16,7 @@ use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 
 /**
- * @author Rafał Mikołajun <rafal@vision-web.pl>
+ * @author Rafał Mikołajun <rafal@mikoweb.pl>
  * @package vSymfo FOSUserBundle
  * @subpackage DependencyInjection
  */
